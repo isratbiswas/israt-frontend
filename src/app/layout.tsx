88c4e440e52d6772fe9073d239a37cd6d4387532
isratbiswas <isratbiswas28@gmail.com>
2026-04-07 12:50:24 +0600

    page loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shered/Navbar";
 import Footer from "@/components/shered/Footer";
+import InitialLoader from "@/components/layout/InitialLoader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <InitialLoader />
         <Navbar />
         <div className="min-h-dvh">{children}</div>
         <Footer />
